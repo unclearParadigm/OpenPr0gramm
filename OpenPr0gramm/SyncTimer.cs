15bc671424de13513be60dc3ac719e95dfc82629
holzmaster <holzmaster@outlook.com>
2016-02-25 02:47:51 +0100

    Add SyncTimer

---
+++ OpenPr0gramm/SyncTimer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace OpenPr0gramm
+{
+    class SyncTimer : IDisposable
+    {
+        public static TimeSpan LowestMeaningfulInterval { get; } = TimeSpan.FromMinutes(1);
+        public static TimeSpan DefaultInterval { get; } = LowestMeaningfulInterval;
+
+        public TimeSpan Interval { get; }
+        public IPr0grammUserService UserService { get; }
+        public int LastId { get; set; } = 0;
+
+        public event EventHandler<SyncResponse> SyncCompleted;
+
+        private Timer _timer;
+        private readonly object _lockObj = new object();
+
+        #region Ctors
+
+        public SyncTimer(IPr0grammApiClient apiClient)
+            : this(GetServiceFromClient(apiClient))
+        { }
+        public SyncTimer(IPr0grammUserService userService)
+            : this(userService, DefaultInterval)
+        { }
+        public SyncTimer(IPr0grammApiClient apiClient, TimeSpan syncInterval)
+            : this(GetServiceFromClient(apiClient), syncInterval)
+        { }
+        public SyncTimer(IPr0grammUserService userService, TimeSpan syncInterval)
+        {
+            if (userService == null)
+                throw new ArgumentNullException(nameof(userService));
+            UserService = userService;
+            Interval = syncInterval < LowestMeaningfulInterval ? LowestMeaningfulInterval : syncInterval;
+        }
+
+        #endregion
+
+        public void Start()
+        {
+            lock (_lockObj)
+            {
+                _timer?.Dispose();
+                _timer = new Timer(async _ => await TimerCallback().ConfigureAwait(false) /* exception stuff. do not touch. */, null, 0, (int)Interval.TotalMilliseconds, true);
+            }
+        }
+
+        public void Stop()
+        {
+            lock (_lockObj)
+            {
+                _timer?.Dispose();
+            }
+        }
+
+        private async Task TimerCallback()
+        {
+            var res = await UserService.Sync(LastId).ConfigureAwait(false);
+            Debug.Assert(res != null);
+            LastId = res.LastId;
+            SyncCompleted?.Invoke(this, res);
+        }
+
+        private static IPr0grammUserService GetServiceFromClient(IPr0grammApiClient client)
+        {
+            if (client == null)
+                throw new ArgumentNullException(nameof(client));
+            return client.User;
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                    _timer?.Dispose();
+                disposedValue = true;
+            }
+        }
+        public void Dispose() => Dispose(true);
+
+        #endregion
+    }
+}
---
+++ OpenPr0gramm/Timer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenPr0gramm
+{
+    /// <see cref="https://stackoverflow.com/a/23567113/785210"/>
+    internal sealed class Timer : CancellationTokenSource
+    {
+        internal Timer(Action<object> callback, object state, int millisecondsDueTime, int millisecondsPeriod, bool waitForCallbackBeforeNextPeriod = false)
+        {
+            Debug.Assert(callback != null);
+            Debug.Assert(millisecondsDueTime >= 0);
+            Debug.Assert(millisecondsPeriod >= 0);
+
+            Task.Delay(millisecondsDueTime, Token).ContinueWith(async (t, s) =>
+            {
+                var tuple = (Tuple<Action<object>, object>)s;
+
+                while (!IsCancellationRequested)
+                {
+                    if (waitForCallbackBeforeNextPeriod)
+                    {
+
+                        tuple.Item1(tuple.Item2);
+                    }
+                    else
+                    {
+#pragma warning disable 4014
+                        Task.Run(() => tuple.Item1(tuple.Item2)); // no wait
+#pragma warning restore
+                    }
+                    await Task.Delay(millisecondsPeriod, Token).ConfigureAwait(false);
+                }
+
+            }, Tuple.Create(callback, state), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.Default);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                Cancel();
+            base.Dispose(disposing);
+        }
+    }
+
+    /*
+    internal sealed class Timer : CancellationTokenSource
+    {
+        public TimeSpan Interval { get; }
+        private readonly TimerCallback _callback;
+        public Timer(TimerCallback callback, TimeSpan interval)
+        {
+            Debug.Assert(callback != null);
+            Interval = interval;
+            _callback = callback;
+        }
+
+        public async Task Run()
+        {
+            while (!IsCancellationRequested)
+            {
+                _callback?.Invoke(Token);
+                await Task.Delay(Interval, Token).ConfigureAwait(false);
+            }
+        }
+    }
+    public delegate void TimerCallback(CancellationToken token);
+    */
+}
