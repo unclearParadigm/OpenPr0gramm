3fb14f5276fed82a5e9b8b94ad9f26b23c2e215c
unclearParadigm <rarepublic@protonmail.com>
2019-10-11 18:32:23 +0200

    stylefixes in unixDateTimeConverter

--- src/OpenPr0gramm/Json/UnixDateTimeConverter.cs
+++ src/OpenPr0gramm/Json/UnixDateTimeConverter.cs
@@ -1,16 +1,19 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
+using Newtonsoft.Json;
+using OpenPr0gramm.Extensions;
 
 namespace OpenPr0gramm.Json
 {
     internal class UnixDateTimeConverter : JsonConverter
     {
         public override bool CanConvert(Type objectType) => objectType == typeof(DateTime);
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
         {
             var t = reader.Value is long value
                 ? value
-                : long.Parse((string)reader.Value);
+                : long.Parse((string) reader.Value);
 
             return DateTimeOffset.FromUnixTimeSeconds(t).DateTime;
         }
@@ -19,7 +22,7 @@ namespace OpenPr0gramm.Json
         {
             if (!(value is DateTime))
                 throw new ArgumentException("Expected date object value.");
-            long ticks = ((DateTime)value).ToUnixTime();
+            long ticks = ((DateTime) value).ToUnixTime();
             writer.WriteValue(ticks);
         }
     }
